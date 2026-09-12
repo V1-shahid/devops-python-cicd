77ccb272bfc8c40912b7320359fcda59ce44c2f2
V1-shahid <shahidvm02@gmail.com>
2026-09-12 15:18:01 +0530

    asdgf

--- test_app.py
+++ test_app.py
@@ -7,7 +7,7 @@ def test_home():
     response = client.get("/")
 
     assert response.status_code == 200
-    assert response.data == b"Hello from DevOps CI/CD - Auto Build v2 pro!"
+    assert response.data == b"Hello from DevOps CI/CD - Auto Build v2 pro Test auto!"
 
 
 def test_health():
